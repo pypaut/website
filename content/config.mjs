606bc6bf08190a88b75c9f4a37a0534214c45c7f
Geoffrey <pypaut@hotmail.fr>
2024-11-04 20:44:33 +0100

    add basic config

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,15 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+
+const config = {
+  title: "My Awesome Blog",
+  description: "This is my awesome blog built with Flowershow",
+  author: "Geoffrey",
+  logo: "/images/logo.svg",
+  domain: "https://geoffreyjount.vercel.app",
+  // links to the pages you want to link to in the navbar and in the footer
+  navLinks: [{ href: "/about", name: "about" }],
+};
+
+export default config;
