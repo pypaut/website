002b273223211293b4ad2e5eaca2cdd17fbe5f6a
Geoffrey <pypaut@hotmail.fr>
2024-11-04 21:01:29 +0100

    add logo

--- content/config.mjs
+++ content/config.mjs
@@ -7,10 +7,15 @@ const config = {
   title: "Geoffrey Jount",
   description: "This is my awesome blog built with Flowershow",
   author: "Geoffrey Jount",
-  logo: "/images/logo.svg",
+  logo: "/images/lol.jpg",
   domain: "https://geoffreyjount.vercel.app",
   // links to the pages you want to link to in the navbar and in the footer
   navLinks: [{ href: "/about", name: "about" }],
+  // navbarTitle: {
+  //   text: "Your custom title here",
+  //   logo: "/images/your-logo.svg", // optional
+  // },
+  showEditLink: false,
 };
 
 export default config;
